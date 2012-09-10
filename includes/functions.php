cfdc6d6e122c119914293be3a5d817f99613ecbd
John James Jacoby <johnjamesjacoby@me.com>
2012-09-10 11:24:34 -0700

    Improve function names in functions.php.

--- includes/functions.php
+++ includes/functions.php
@@ -1,5 +1,23 @@
 <?php
 
-function flux_get_id() {
+/**
+ * Are we looking at a flux query?
+ *
+ * @since Flux (0.1)
+ * @global WP_Query $wp_query
+ * @return bool
+ */
+function is_flux_query() {
+	global $wp_query;
+
+	return (bool) isset( $wp_query->flux );
+}
+
+/**
+ * Get the 
+ * @since Flux (0.1)
+ * @return string
+ */
+function flux_get_rewrite_id() {
 	return apply_filters( 'flux_get_id', 'flux_id' );
 }
