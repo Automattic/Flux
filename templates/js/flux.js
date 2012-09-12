c7f987173b2ea3a2358106401510c5a097427abc
John James Jacoby <johnjamesjacoby@me.com>
2012-09-12 12:55:17 -0700

    Tweak flux.js code whitespace.

--- templates/js/flux.js
+++ templates/js/flux.js
@@ -1,10 +1,14 @@
 jQuery( document ).ready( function() {
-	var flux = jQuery('#flux-capacitor');
+
+	var flux = jQuery( '#flux-capacitor' );
+
 	if ( !! flux.offset() ) {
+
 		var stickyTop = flux.offset().top;
 		var topGap    = 28;
 
-		jQuery(window).scroll( function() {
+		jQuery( window ).scroll( function() {
+
 			var windowTop = jQuery( window ).scrollTop();
 
 			if ( stickyTop - topGap < windowTop ) {
