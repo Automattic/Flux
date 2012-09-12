78cccaf378b95050f4187e9e6d73521cc04699d1
John James Jacoby <johnjamesjacoby@me.com>
2012-09-12 12:54:30 -0700

    Filter post and body classes into new sub-filters.

--- includes/filters.php
+++ includes/filters.php
@@ -44,10 +44,12 @@ add_filter( 'request',                 'flux_request',            10    );
 add_filter( 'template_include',        'flux_template_include',   10    );
 add_filter( 'wp_title',                'flux_title',              10, 3 );
 add_filter( 'body_class',              'flux_body_class',         10, 2 );
+add_filter( 'post_class',              'flux_post_class',         10, 2 );
 add_filter( 'sidebars_widgets',        'flux_sidebar_widgets',    10    );
 
 // Template Compatibility
 add_filter( 'flux_template_include', 'flux_template_include_theme_compat',   4, 2 );
 
-// Queries
-//add_filter( 'posts_request', '_flux_has_replies_where', 10, 2 );
+// Theme Classes
+add_filter( 'flux_body_class', 'flux_add_body_class', 10, 2 );
+add_filter( 'flux_post_class', 'flux_add_post_class', 10, 3 );
